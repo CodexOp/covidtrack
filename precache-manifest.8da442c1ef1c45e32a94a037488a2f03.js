80879a40812747bdbfe8932aa28a55e8f28471f0
Beast-Op <66095166+Beast-Op@users.noreply.github.com>
2020-10-19 20:22:27 +0530

    Updates

---
+++ precache-manifest.8da442c1ef1c45e32a94a037488a2f03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74f28bfb72d419df8df268c89a0071d8",
+    "url": "/covidtrack/index.html"
+  },
+  {
+    "revision": "08723b9451f28d79bace",
+    "url": "/covidtrack/static/css/main.6a4aecdd.chunk.css"
+  },
+  {
+    "revision": "60167d16bf9185aff32f",
+    "url": "/covidtrack/static/js/2.528e0b28.chunk.js"
+  },
+  {
+    "revision": "480a7794cb91dfaf84868feef3f75220",
+    "url": "/covidtrack/static/js/2.528e0b28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08723b9451f28d79bace",
+    "url": "/covidtrack/static/js/main.da14057f.chunk.js"
+  },
+  {
+    "revision": "1ee33134c8fafacfb8fb",
+    "url": "/covidtrack/static/js/runtime-main.ec35d52b.js"
+  },
+  {
+    "revision": "f5d4ea53d69abc74057b9c0513baa507",
+    "url": "/covidtrack/static/media/main.f5d4ea53.png"
+  }
+]);
